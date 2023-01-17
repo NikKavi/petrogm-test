4293569ab9f596cbb37ec84a4690928e2235b109
ИВ-023 Кавыршин Николай <kolyancik02@gmail.com>
2023-01-18 01:04:41 +0700

    Class User Created

---
+++ SharedProject1/Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+
+namespace SharedProject1.Models
+{
+    class User : Person
+    {
+        public User(string username) : base(username) 
+        {
+            Phrases = new string[] {"Hey"};
+        }
+        
+        public override void Talk()
+        {
+            Console.WriteLine(Username + ": " + Phrases[0]);
+        }
+        
+    }
+}
---
+++ petrotest/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using SharedProject1.Models;
+
+namespace petrotest
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string userName = "UserName";
+            string juniorName = "JuniorName";
+            User user = new(userName);
+            user.Talk();
+            Junior junior = new(juniorName);
+            junior.Talk();
+        }
+    }
+}
