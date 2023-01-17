f66be7908d4108d4b46e6d3bc1cb417c639af09b
ИВ-023 Кавыршин Николай <kolyancik02@gmail.com>
2023-01-18 00:45:20 +0700

    Class Person created

---
+++ SharedProject1/Models/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharedProject1.Models
+{
+    abstract class Person
+    {
+        public string? Username { get; set; }
+        public string? Role { get; set; }
+        public int RoleId { get; set; }
+        public string[]? Phrases;
+
+        public Person(string username)
+        {
+            Username = username;
+        }
+        public abstract void Talk();
+
+    }
+}
