da5f6b749edb6202df34077371bb4081c170f8d6
ИВ-023 Кавыршин Николай <kolyancik02@gmail.com>
2023-01-18 16:15:26 +0700

    Updated Program

--- petrotest/Program.cs
+++ petrotest/Program.cs
@@ -25,7 +25,7 @@ namespace petrotest
             string[] sequence = FileReading();
             int personCount = Array.FindAll(sequence, x => x.Contains(' ')).Length;
             Console.WriteLine($"PersonCount: {personCount}");
-            string[] roles = { "User", "Junior" };
+            string[] roles = { "User", "Junior", "Middle", "Senior" };
             int[] roleCounts = new int[roles.Length]; //используем для подсчёта кол-ва юзеров, джунов и тд.
             for (int i = 0; i < roles.Length; i++)
             {
@@ -34,24 +34,72 @@ namespace petrotest
             }
             User[] users = new User[roleCounts[0]];
             Junior[] juniors = new Junior[roleCounts[1]];
+            Middle[] middles = new Middle[roleCounts[2]];
+            Senior[] seniors = new Senior[roleCounts[3]];
            
             Array.Clear(roleCounts,0, roleCounts.Length); //очищаем, а затем повторно используем для индексации массивов экземпляров классов
             string[] person = new string[2];
- 
-            for(int i = 0; i < personCount; i++)
+            
+            for(int i = 0; i < personCount; i++) //создание экземпляра классов
             {
                 person = sequence[i].Split(' ');
                 switch (person[1])
                 {
                     case "User":
                         users[roleCounts[0]] = new User(person[0]);
-                        users[roleCounts[0]].Talk();
                         roleCounts[0]++;
                         break;
                     case "Junior":
                         juniors[roleCounts[1]] = new Junior(person[0]);
+                        roleCounts[1]++;
+                        break;
+                    case "Middle":
+                        middles[roleCounts[2]] = new Middle(person[0]);
+                        roleCounts[2]++;
+                        break;
+                    case "Senior":
+                        seniors[roleCounts[3]] = new Senior(person[0]);
+                        roleCounts[3]++;
+                        break;
+                }
+            }
+            for(int i = 0; i < juniors.Length; i++)
+            {
+                for(int j = 0; j < seniors.Length; j++)
+                {
+                    juniors[i].TriggerJuniorMessage += seniors[j].RaiseFlag;
+                }
+            }
+
+            for (int i = 0; i < middles.Length; i++)
+            {
+                for (int j = 0; j < seniors.Length; j++)
+                {
+                    middles[i].TriggerMiddleMessage += seniors[j].RaiseFlag;
+                }
+            }
+            Array.Clear(roleCounts, 0, roleCounts.Length); //обнуляем индексаторы
+            for (int i = 0; i < personCount; i++) //здесь происходит общение
+            {
+                person = sequence[i].Split(' ');
+                switch (person[1])
+                {
+                    case "User":
+                        users[roleCounts[0]].Talk();
+                        roleCounts[0]++;
+                        break;
+                    case "Junior":
                         juniors[roleCounts[1]].Talk();
                         roleCounts[1]++;
+                        break;
+                    case "Middle":
+
+                        middles[roleCounts[2]].Talk();
+                        roleCounts[2]++;
+                        break;
+                    case "Senior":
+                        seniors[roleCounts[3]].Talk();
+                        roleCounts[3]++;
                         break;
                 }
             }
