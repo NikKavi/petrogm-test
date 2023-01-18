e4485174d14db99be97e349fb8326de6965988ab
ИВ-023 Кавыршин Николай <kolyancik02@gmail.com>
2023-01-18 18:47:21 +0700

    Created TeamLead class

---
+++ SharedProject1/Models/TeamLead.cs
@@ -0,0 +1,39 @@
+﻿namespace SharedProject1.Models
+{
+     class TeamLead : Person
+    {
+        private int reactionFlag = 0;
+
+        private string triggerName;
+        public TeamLead(string username) : base(username)
+        {
+            Phrases = new string[] { "Hey", "Of course, baby. Be ready to suffer " };
+        }
+
+        public override void Talk()
+        {
+            Console.WriteLine(Username + ": " + Phrases[0]);
+            if (reactionFlag >= 1)
+            {
+                Reaction();
+                reactionFlag = 0;
+            }
+        }
+
+        private void Reaction()
+        {
+
+            Console.WriteLine(Username + ": " + Phrases[1] + triggerName + "!");
+        }
+
+        public void SetTriggerName(string name)
+        {
+            triggerName= name;
+        }
+
+        public int RaiseFlag()
+        {
+            return reactionFlag++;
+        }
+    }
+}
