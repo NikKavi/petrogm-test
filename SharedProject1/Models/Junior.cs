70d44ee717823216780ab9784d813ef43f358050
ИВ-023 Кавыршин Николай <kolyancik02@gmail.com>
2023-01-18 16:15:09 +0700

    Updated Junior class

--- SharedProject1/Models/Junior.cs
+++ SharedProject1/Models/Junior.cs
@@ -6,6 +6,7 @@ namespace SharedProject1.Models
 {
     class Junior : Person
     {
+        public event Message TriggerJuniorMessage;
         public Junior(string username) : base(username)
         {
             Phrases = new string[] { "Hey", "I want a merge. Will somebody review it for me?" };
@@ -15,6 +16,7 @@ namespace SharedProject1.Models
         {
             Console.WriteLine(Username + ": " + Phrases[0]);
             Console.WriteLine(Username + ": " + Phrases[1]);
+            TriggerJuniorMessage();
         }
 
     }
