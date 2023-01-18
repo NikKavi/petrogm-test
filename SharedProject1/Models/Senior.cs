5fd5851084b1f3f21148e25e8a02929e42bfa7d7
ИВ-023 Кавыршин Николай <kolyancik02@gmail.com>
2023-01-18 16:14:29 +0700

    Created Senior class

---
+++ SharedProject1/Models/Senior.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharedProject1.Models
+{
+    class Senior : Person
+    {
+        private int reactionFlag = 0;
+        public Senior(string username) : base(username)
+        {
+            Phrases = new string[] { "Hey", "You are such a jnr! Are you afraid of the Team Lead?" };
+        }
+
+        public override void Talk()
+        {
+            Console.WriteLine(Username + ": " + Phrases[0]);
+            if(reactionFlag > 1)
+            {
+                Reaction();
+                reactionFlag = 0;
+            }
+        }
+
+        public void Reaction()
+        {
+
+            Console.WriteLine(Username + ": " + Phrases[1]);
+        }
+
+        public int RaiseFlag()
+        {
+            return reactionFlag++;
+        }
+    }
+}
